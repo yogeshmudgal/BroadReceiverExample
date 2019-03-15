b22fb5066121fe20ff4fe8d62ce73d4240fce96d
Yogesh Mudgal <yogesh.jags@gmail.com>
2019-03-15 21:16:44 +0530

    First Commit

---
+++ app/src/main/java/com/example/broadcastreceiverexample/MyBroadCastReceiver.java
@@ -0,0 +1,22 @@
+package com.example.broadcastreceiverexample;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class MyBroadCastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+            Toast.makeText(context,"Boot Completed",Toast.LENGTH_LONG).show();
+        }
+
+        if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+            Toast.makeText(context,"Boot Completed",Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
